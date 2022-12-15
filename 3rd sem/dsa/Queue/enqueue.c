497576447635466e5f19ebad5dbf7eb9910b0e42
Yakumaa <maharjanshrish8@gmail.com>
2022-12-15 22:42:01 +0545

    fixed stuffs

--- 3rd sem/dsa/Queue/enqueue.c
+++ 3rd sem/dsa/Queue/enqueue.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
 #define SIZE 5
 
-int item[SIZE], front = 0, rear = -1;
+int item[SIZE], front = -1, rear = -1;
 
 void enqueue(int value){
-    if (rear >= SIZE - 1)
+    if (rear == SIZE - 1)
     {
         printf("Queue is full");
     }
     else
+    if (front == -1)
+    {
+        front = 0;
+    }
+    
         rear = rear + 1;
         item[rear] = value;
         printf("\nInserted -> %d", value);
 }
 
 void printQueue(){
-    printf("Queue: ");
+    printf("\nQueue: ");
     for (int i = front; i <= rear; i++)
     {
         printf("%d ", item[i]);
