5c90037e84eec75dd3fe28daf0677a6332925d23
Yakumaa <maharjanshrish8@gmail.com>
2022-12-21 22:09:45 +0545

    first commit

---
+++ 3rd sem/dsa/Recursion/gcd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int n1,int n2){
+    if (n2 != 0)
+    {
+        return gcd(n2, n1%n2);
+    }
+    else
+        return n1;
+    
+}
+int main(){
+    int n1, n2;
+    cout << "Enter two positive numbers: ";
+    cin >> n1 >> n2;
+    cout << "The gcd of " << n1 << " and " << n2 << " is " << gcd(n1, n2);
+    return 0;
+}
