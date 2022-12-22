531a1cc60d40a86c3ad67fb71efe08d0cde61ca5
Yakumaa <maharjanshrish8@gmail.com>
2022-12-22 19:24:07 +0545

    fixed minor stuffs

--- 3rd sem/dsa/Lists/insertion_beginning.c
+++ 3rd sem/dsa/Lists/insertion_beginning.c
@@ -7,14 +7,14 @@ struct node
     struct node *next;
 };
 typedef struct node NodeType;
-NodeType *head;
 
-void display(struct node* n)
+
+void display(struct node* head)
 {
-    while (n != NULL)
+    while (head != NULL)
     {
-        printf("%d ", n -> info);
-        n = n -> next;
+        printf("%d ", head -> info);
+        head = head -> next;
     }
     
 };
@@ -22,6 +22,7 @@ void display(struct node* n)
 int main(){
     NodeType *NewNode;
     NodeType *first, *last;
+    NodeType *head = NULL;
     NewNode = (NodeType*)malloc(sizeof(NodeType));
     first = (NodeType*)malloc(sizeof(NodeType));
     last = (NodeType*)malloc(sizeof(NodeType));
@@ -37,3 +38,56 @@ int main(){
     display(head);
     return 0;
 }
+
+
+////////////////////////CHATGPT/////////////////////////////
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// // Define a structure for the nodes of the linked list
+// struct Node {
+//   int data;
+//   struct Node* next;
+// };
+
+// // Function to insert a node at the beginning of the linked list
+// void insertAtBeginning(struct Node** head_ref, int new_data) {
+//   // Allocate memory for the new node
+//   struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+//   // Set the data of the new node
+//   new_node->data = new_data;
+
+//   // Set the next pointer of the new node to point to the current head of the list
+//   new_node->next = (*head_ref);
+
+//   // Set the head of the list to point to the new node
+//   (*head_ref) = new_node;
+// }
+
+// // Function to print the linked list
+// void printList(struct Node* head) {
+//   // Iterate through the list and print each node's data
+//   while (head != NULL) {
+//     printf("%d ", head->data);
+//     head = head->next;
+//   }
+//   printf("\n");
+// }
+
+// int main() {
+//   // Initialize an empty linked list
+//   struct Node* head = NULL;
+
+//   // Insert some nodes at the beginning of the list
+//   insertAtBeginning(&head, 5);
+//   insertAtBeginning(&head, 4);
+//   insertAtBeginning(&head, 3);
+//   insertAtBeginning(&head, 2);
+//   insertAtBeginning(&head, 1);
+
+//   // Print the linked list
+//   printList(head);
+
+//   return 0;
+// }
