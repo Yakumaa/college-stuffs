6a2cfbbfce67ba58534656fcca020adc8b4c320d
Yakumaa <maharjanshrish8@gmail.com>
2024-07-04 21:16:52 +0545

    first and follow

--- 6th sem/CDC/first.c
+++ 6th sem/CDC/first.c
@@ -1,108 +1,103 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
-#define MAX 100
-#define MAX_PRODUCTIONS 10
+int numOfProductions;
+char productionSet[10][10];
 
-// Structure to hold the FIRST sets
-struct FirstSet {
-    char nonTerminal;
-    char first[MAX];
-};
+void FIRST(char[], char);
+void addToResultSet(char[], char);
 
-struct FirstSet firstSets[MAX];
-int firstSetCount = 0;
+int main()
+{
+    int i;
+    char choice, c, result[20];
 
-// Helper function to add a symbol to the FIRST set
-void addToFirstSet(char nonTerminal, char symbol) {
-    for (int i = 0; i < firstSetCount; i++) {
-        if (firstSets[i].nonTerminal == nonTerminal) {
-            if (!strchr(firstSets[i].first, symbol)) {
-                int len = strlen(firstSets[i].first);
-                firstSets[i].first[len] = symbol;
-                firstSets[i].first[len + 1] = '\0';
-            }
-            return;
-        }
+    printf("How many number of productions?: ");
+    scanf(" %d", &numOfProductions);
+
+    for (i = 0; i < numOfProductions; i++)
+    {
+        printf("Enter productions Number %d: ", i + 1);
+        scanf(" %s", productionSet[i]);
     }
-    firstSets[firstSetCount].nonTerminal = nonTerminal;
-    firstSets[firstSetCount].first[0] = symbol;
-    firstSets[firstSetCount].first[1] = '\0';
-    firstSetCount++;
-}
 
-// Function to find the FIRST set of a non-terminal
-void findFirst(char grammar[][MAX], char nonTerminal, int grammarCount) {
-    for (int i = 0; i < grammarCount; i++) {
-        if (grammar[i][0] == nonTerminal && grammar[i][1] == '-' && grammar[i][2] == '>') {
-            for (int j = 3; j < strlen(grammar[i]); j++) {
-                char symbol = grammar[i][j];
-                if (symbol == ' ' || symbol == '|') {
-                    continue;
-                }
-                if (islower(symbol) || isdigit(symbol) || symbol == '(' || symbol == ')' || symbol == '+' || symbol == '*' || symbol == '/' || symbol == '-') {
-                    addToFirstSet(nonTerminal, symbol);
-                    break;
-                } else if (isupper(symbol)) {
-                    findFirst(grammar, symbol, grammarCount);
-                    for (int k = 0; k < firstSetCount; k++) {
-                        if (firstSets[k].nonTerminal == symbol) {
-                            for (int l = 0; l < strlen(firstSets[k].first); l++) {
-                                if (firstSets[k].first[l] != 'ε') {
-                                    addToFirstSet(nonTerminal, firstSets[k].first[l]);
-                                }
-                            }
-                            if (!strchr(firstSets[k].first, 'ε')) {
-                                break;
-                            }
-                        }
-                    }
-                    if (!strchr(firstSets[firstSetCount - 1].first, 'ε')) {
-                        break;
-                    }
-                } else if (symbol == 'ε') {
-                    addToFirstSet(nonTerminal, symbol);
-                    break;
-                }
-            }
-        }
-    }
-}
-
-// Function to display the FIRST sets
-void displayFirstSets() {
-    printf("FIRST sets:\n");
-    for (int i = 0; i < firstSetCount; i++) {
-        printf("FIRST(%c) = { ", firstSets[i].nonTerminal);
-        for (int j = 0; j < strlen(firstSets[i].first); j++) {
-            printf("%c ", firstSets[i].first[j]);
-        }
+    do
+    {
+        printf("\nFind the FIRST of: ");
+        scanf(" %c", &c);
+        FIRST(result, c); // Compute FIRST; Get Answer in 'result' array
+        printf("\nFIRST(%c) = { ", c);
+        for (i = 0; result[i] != '\0'; i++)
+            printf("%c ", result[i]); // Display result
         printf("}\n");
-    }
-}
-
-int main() {
-    // Define the grammar
-    char grammar[MAX_PRODUCTIONS][MAX] = {
-        "E -> TX",
-        "X -> +TX",
-        "T -> FY",
-        "Y -> *FY",
-        "F -> (E) | id"
-    };
-    
-    int grammarCount = 5;
-    char nonTerminals[] = "EXYTF";
-    int n = strlen(nonTerminals);
-
-    // Find FIRST sets for each non-terminal
-    for (int i = 0; i < n; i++) {
-        findFirst(grammar, nonTerminals[i], grammarCount);
-    }
-
-    // Display the FIRST sets
-    displayFirstSets();
+        printf("Press 'y' to continue: ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
 
     return 0;
 }
+
+void FIRST(char *Result, char c)
+{
+    int i, j, k;
+    char subResult[20];
+    int foundEpsilon;
+
+    subResult[0] = '\0';
+    Result[0] = '\0';
+
+    // If X is terminal, FIRST(X) = {X}
+    if (!(isupper(c)))
+    {
+        addToResultSet(Result, c);
+        return;
+    }
+
+    // If X is non-terminal then read each production
+    for (i = 0; i < numOfProductions; i++)
+    {
+        // Find production with X as LHS
+        if (productionSet[i][0] == c)
+        {
+            if (productionSet[i][2] == '$') // epsilon production
+                addToResultSet(Result, '$');
+            else
+            {
+                j = 2;
+                while (productionSet[i][j] != '\0')
+                {
+                    foundEpsilon = 0;
+                    FIRST(subResult, productionSet[i][j]);
+                    for (k = 0; subResult[k] != '\0'; k++)
+                        addToResultSet(Result, subResult[k]);
+                    for (k = 0; subResult[k] != '\0'; k++)
+                    {
+                        if (subResult[k] == '$')
+                        {
+                            foundEpsilon = 1;
+                            break;
+                        }
+                    }
+                    // No epsilon found, no need to check next element
+                    if (!foundEpsilon)
+                        break;
+                    j++;
+                }
+            }
+        }
+    }
+    return;
+}
+
+void addToResultSet(char Result[], char val)
+{
+    int k;
+    for (k = 0; Result[k] != '\0'; k++)
+    {
+        if (Result[k] == val)
+            return;
+    }
+    Result[k] = val;
+    Result[k + 1] = '\0';
+}
+
